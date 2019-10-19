0dc1dae341b774711afde04d1db93529026204fe
CalmDuane <jonpyles97@gmail.com>
2019-10-19 15:46:06 -0400

    Yeet

--- QR_Contacts/src/Test.java
+++ QR_Contacts/src/Test.java
@@ -1,6 +1,6 @@
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("Big yeet gang 2: the return of yeetasaurus");
+		System.out.println("Big yeet gang 2.half: the return of Jon from his nap");
 	}
 }
